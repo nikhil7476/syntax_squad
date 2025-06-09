8b4b8b3c6f101afab55a359ce692c5ea90b20203
nikhil7476 <nikhil9027917476@gmail.com>
2025-06-09 21:53:16 +0530

    new file:   src/components/BlogCards.js 	new file:   src/components/ProjectCards.js 	new file:   src/components/ServiceCards.js

---
+++ src/components/BlogCards.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function BlogCards() {
+  return (
+    <>
+      <div>BlogCards</div>
+    </>
+  );
+}
+
+export default BlogCards;
---
+++ src/components/ProjectCards.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ProjectCards() {
+  return (
+    <>
+      <div>ProjectCards</div>
+    </>
+  );
+}
+
+export default ProjectCards;
